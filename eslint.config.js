969c8871911b9b354b2fb689b45b1f1c0add9b5c
applevalley <skatkd7571@naver.com>
2024-12-31 12:39:08 +0900

    chore(weather): add eslint/typescript/prettier settings

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,28 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js";
+import globals from "globals";
+import eslintReactPluginHooks from "eslint-plugin-react-hooks";
+import eslintReactPlugin from "eslint-plugin-react";
+import eslintReactPluginRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ["dist", "node_modules"] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{js, jsx, ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      "react-hooks": eslintReactPluginHooks,
+      "react-refresh": eslintReactPluginRefresh,
+      react: eslintReactPlugin,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      ...eslintReactPlugin.configs.flat.recommended.rules,
+      ...eslintReactPluginHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
     },
-  },
-)
+  }
+);
