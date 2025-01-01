380b3e16cff14fe8ea64be60238249ac3c3cece5
applevalley <skatkd7571@naver.com>
2025-01-02 01:03:30 +0900

    feat(weather): add router/update App.tsx with queryClient

---
+++ src/routes/Route.tsx
@@ -0,0 +1,19 @@
+import { Navigate } from "react-router-dom";
+
+import { PAGE_URL } from "@/src/constants/path";
+
+import Layout from "@/src/components/Layout";
+import WeatherPage from "@/src/components/pages/WeatherPage";
+
+// 라우터 컴포넌트
+export default function Route() {
+  return [
+    {
+      element: <Layout />,
+      children: [
+        { path: PAGE_URL.MAIN, element: <WeatherPage /> },
+        { path: "*", element: <Navigate to={PAGE_URL.MAIN} replace /> }, // 그 외 페이지에 직접 접근하려는 경우, redirect해줍니다.
+      ],
+    },
+  ];
+}
