2ef00d65d22178182128f116a00fd3e7ec5e50b2
applevalley <skatkd7571@naver.com>
2025-01-02 16:48:31 +0900

    feat(weather): update header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,11 @@
 // 헤더 컴포넌트
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="h-10 bg-slate-300 flex justify-between items-center px-4">
+      <h5 className="text-xl">일기예보 </h5>
+      <h5 className="text-xl">안녕하세요, user</h5>
+    </div>
+  );
 };
 
 export default Header;
