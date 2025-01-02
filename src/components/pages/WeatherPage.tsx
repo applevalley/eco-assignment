e88c1c3151db909924760eb1a94db18ef3976507
applevalley <skatkd7571@naver.com>
2025-01-02 16:48:51 +0900

    feat(weather): update main page component

--- src/components/pages/WeatherPage.tsx
+++ src/components/pages/WeatherPage.tsx
@@ -1,6 +1,24 @@
+import { useState } from "react";
+
+import NowWeather from "@/components/organisms/NowWeather";
+import TodayWeather from "@/components/organisms/TodayWeather";
+import LocationSearch from "@/components/organisms/LocationSearch";
+
+interface locationProps {
+  gridX: number;
+  gridY: number;
+}
+
 // 메인 페이지 (날씨 검색)
 const WeatherPage = () => {
-  return <div>WeatherPage</div>;
+  const [selectedLocation, setSelectedLocation] = useState<locationProps | null>(null);
+  return (
+    <div className="space-y-4">
+      <NowWeather />
+      <TodayWeather />
+      <LocationSearch onLocationSelect={(location) => setSelectedLocation(location)} />
+    </div>
+  );
 };
 
 export default WeatherPage;
