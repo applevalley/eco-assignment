769d3d8d15c7ff1f309ef8749aa0c3cc6ec08ea2
applevalley <skatkd7571@naver.com>
2025-01-02 16:46:47 +0900

    feat(weather): update api url constants

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import "./App.css";
-import Route from "@/src/routes/Route";
+import Route from "@/routes/Route";
 
 function App() {
   const queryClient = new QueryClient({
--- src/constants/path.ts
+++ src/constants/path.ts
@@ -3,3 +3,16 @@ export const PAGE_URL = {
   MAIN: "/", // 메인 페이지
   FAVORITE: `/investment`, // 즐겨찾기 페이지
 };
+
+// 각 API 주소를 api 호출부에서 직접 입력하지 않게 하기 위한 상수 선언
+export const API_URL = {
+  ULTRA_WEATHER_LIVE: "/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst", // 초단기실황조회
+  ULTRA_WEATHER_FORECAST: "/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst", // 초단기예보조회
+  WEATHER_FORECAST: "/1360000/VilageFcstInfoService_2.0/getVilageFcst", // 단기예보조회
+  FORECAST_VERSION: "/1360000/VilageFcstInfoService_2.0/getFcstVersion", // 예보버전조회
+
+  MID_FORECAST: "/1360000/MidFcstInfoService/getMidFcst", // 중기전망조회
+  MID_LAND_FORECAST: "/1360000/MidFcstInfoService/getMidLandFcst", // 중기육상예보조회
+  MID_TEMPERATURE: "/1360000/MidFcstInfoService/getMidTa", // 중기기온조회
+  MID_SEA: "/1360000/MidFcstInfoService/getMidSeaFcst", // 중기해상예보조회
+};
