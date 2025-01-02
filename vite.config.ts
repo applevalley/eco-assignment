1e0436581afe3c36f05736cd0b07830bbab68d88
applevalley <skatkd7571@naver.com>
2025-01-02 16:44:46 +0900

    chore(weather): add location data for search/update tailwind config/add proxy config to avoid cors error

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,20 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // 기상청 API의 기본 경로에 대한 프록시 설정
+      "/1360000": {
+        target: "http://apis.data.go.kr",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
 });
